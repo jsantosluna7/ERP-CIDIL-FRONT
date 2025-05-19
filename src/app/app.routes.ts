73f51fc5167924c22cb38049ef8508d1ab062c99
Joan Santos <jsantosluna06@gmail.com>
2025-05-19 10:25:28 -0400

    Creando proyecto de front, y agregando componentes para el inicio de sesion, ademas de agregar las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: 'home/login', pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./components/inicio-sesion/login-layout/login-layout.component').then(m => m.LoginLayoutComponent),
+        children: [
+            {path: 'login', loadComponent: () => import('./components/inicio-sesion/login/login.component').then(m => m.LoginComponent)},
+            {path: 'registrar', loadComponent: () => import('./components/inicio-sesion/registro/registro.component').then(m => m.RegistroComponent)},
+            {path: 'recuperar-contrasena', loadComponent: () => import('./components/inicio-sesion/recuperar-contrasena/recuperar-contrasena.component').then(m => m.RecuperarContrasenaComponent)},
+            {path: 'cambiar-contrasena/:token', loadComponent: () => import('./components/inicio-sesion/cambiar-contrasena/cambiar-contrasena.component').then(m => m.CambiarContrasenaComponent)},
+        ],
+    }
+];
