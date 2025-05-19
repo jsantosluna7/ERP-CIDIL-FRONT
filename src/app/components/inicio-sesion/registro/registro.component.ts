09836ad9c8409922e4794981116c802477c7cbd3
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2025-05-19 16:32:25 -0400

    Arreglando la parte de registro, achicacando los inputs.

--- src/app/components/inicio-sesion/registro/registro.component.ts
+++ src/app/components/inicio-sesion/registro/registro.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+registroForm: FormGroup;
 
+constructor(){
+  this.registroForm = new FormGroup({
+    email: new FormControl('', [Validators.required,Validators.email]),
+    contrasena: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    nombre: new FormControl('',[Validators.required]),
+    apellido: new FormControl('',[Validators.required]),
+    matricula: new FormControl('',[Validators.required]),
+    telefono: new FormControl('',[Validators.required]),
+    direccion: new FormControl('',[Validators.required])
+
+  });
+}
+
+ register(){
+  console.log(this.registroForm.value);
+ }
 }
