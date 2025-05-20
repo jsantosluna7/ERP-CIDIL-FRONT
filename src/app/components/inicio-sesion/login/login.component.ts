6019760a5d996afac2f871bcdc3cd3026ef62627
Joan Santos <jsantosluna06@gmail.com>
2025-05-20 09:35:58 -0400

    Agregando iconos al inicio de sesion y modificando margenes del registro.

--- src/app/components/inicio-sesion/login/login.component.ts
+++ src/app/components/inicio-sesion/login/login.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  faEnvelope = faEnvelope;
+  faLock = faLock
+
+
+  // FormGroup para el formulario de inicio de sesión
+  // Se utiliza ReactiveFormsModule para la gestión de formularios reactivos
+  // Se importan los módulos necesarios desde Angular y FontAwesome
   loginForm: FormGroup;
 
   constructor() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       contrasena: new FormControl('', [Validators.required, Validators.minLength(6)]),
       // rememberMe: new FormControl(false)
     });
