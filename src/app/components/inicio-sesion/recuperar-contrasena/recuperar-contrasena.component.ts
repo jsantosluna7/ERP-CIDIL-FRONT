ca7e5e071654443c21adb048936bc555a9360520
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2025-05-19 20:39:19 -0400

    Recuperar contraseña-correo

--- src/app/components/inicio-sesion/recuperar-contrasena/recuperar-contrasena.component.ts
+++ src/app/components/inicio-sesion/recuperar-contrasena/recuperar-contrasena.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recuperar-contrasena',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './recuperar-contrasena.component.html',
   styleUrl: './recuperar-contrasena.component.css'
 })
 export class RecuperarContrasenaComponent {
 
+  recuperarContrasenaForm: FormGroup;
+
+  constructor(){
+    this.recuperarContrasenaForm = new FormGroup({
+       email: new FormControl('',[Validators.required,Validators.email]),
+    });
+  }
+
+  register(){
+  console.log(this.recuperarContrasenaForm.value);
+ }
 }
