c60db2b2bb1eb67a1fbd2c5af5485cf595668659
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2025-05-20 14:50:43 -0400

    Se mejoro el registro de usuarios para que se pueda confirmar la contrasena

--- src/app/components/inicio-sesion/registro/registro.component.ts
+++ src/app/components/inicio-sesion/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faLocationDot, faLock, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
 
@@ -25,16 +25,39 @@ constructor(){
   this.registroForm = new FormGroup({
     email: new FormControl('', [Validators.required,Validators.email]),
     contrasena: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    confirmarcontrasena: new FormControl('', [Validators.required, Validators.minLength(6)]),
     nombre: new FormControl('',[Validators.required]),
     apellido: new FormControl('',[Validators.required]),
     matricula: new FormControl('',[Validators.required]),
     telefono: new FormControl('',[Validators.required]),
     direccion: new FormControl('',[Validators.required])
-
+    
+  },{
+    validators:this.matchContrasena 
   });
 }
 
  register(){
   console.log(this.registroForm.value);
  }
+
+//Validar contrasena
+matchContrasena : ValidatorFn = (control: AbstractControl):ValidationErrors|null =>{
+
+let contrasena = control.get('contrasena');
+let confirmarcontrasena = control.get('confirmarcontrasena');
+if(contrasena && confirmarcontrasena && contrasena?.value != confirmarcontrasena?.value){
+return{
+  contrasenaError : true
+}
+
+}
+return null;
+
+}
+
+
+
+
+
 }
