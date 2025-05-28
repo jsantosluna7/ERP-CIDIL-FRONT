0a15031b9b2a5431c9ba08f1bd1af5c2b44bce09
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2025-05-28 11:43:36 -0400

    creacion del navbar

--- src/app/components/elements/navbar/navbar.component.ts
+++ src/app/components/elements/navbar/navbar.component.ts
@@ -1,50 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowRight,faClock, faArrowRightFromBracket, faBell, faCartShopping, faChartSimple, faCreditCard, faDesktop, faGreaterThan, faHeart, faHouse, faMagnifyingGlass, faMicrochip, faShop, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FontAwesomeModule, CommonModule, RouterLink],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
-   arrowright = faGreaterThan;
-   house = faHouse;
-   solid =faMagnifyingGlass;
-   heart =faDesktop;
-   regular =faChartSimple;
-   bell = faBell;
-   credi = faUser;
-   car = faShop;
-   left = faArrowRightFromBracket;
-   iot =faMicrochip;
-   faclok = faClock;
-   carro =faCartShopping;
-
-  // body = document.querySelector(".body");k
-  //  sidebar=this.body?.querySelector(".sidebar");
-  //  toggle = this.body?.querySelector(".toggle");
-  //  searchBtn = this.body?.querySelector(".search-box");
-
-  // toogle(event: any){
-  //   const elemento:any = document.querySelector(".sidebar");
-  //   // console.log(elemento)
-  //   elemento.classList.toggle("close");
-
-  // }
-
-  @Output() toggleSidebar = new EventEmitter<void>();
-
-  onToggleClick(){
-    this.toggleSidebar.emit();
-    const elemento:any = document.querySelector(".sidebar");
-    elemento.classList.toggle("close");
-  }
-
-
 
 }
