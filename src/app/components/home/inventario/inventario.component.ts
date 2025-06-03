660f80152d99a2809287670cdf2a007d0514ab98
Joan Santos <jsantosluna06@gmail.com>
2025-06-03 14:25:40 -0400

    Agregando el servicio para el invenatrio

--- src/app/components/home/inventario/inventario.component.ts
+++ src/app/components/home/inventario/inventario.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Carta } from '../../../interfaces/carta';
-import { InventarioService } from './inventario.service';
 import { CommonModule } from '@angular/common';
-import { CarritoService } from '../carrito/carrito.service';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { InventarioService } from '../../../services/Inventario/inventario.service';
 
 @Component({
   selector: 'app-inventario',
@@ -21,64 +19,31 @@ import { MatIconModule } from '@angular/material/icon';
     MatSortModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule, ],
+    MatIconModule,],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css'
 })
 export class InventarioComponent implements OnInit {
-  cartas: Carta[] = [];
-  cartasFiltradas: Carta[] =[];
 
-  
-  /* ---------- Paginacion ------------ */
-
-  pageSize = 20;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  datos: any[] = [];
 
 
-  constructor(private inventarioService: InventarioService, private carritoService: CarritoService){}
+  constructor(private _inventario: InventarioService) { }
 
-  dataSource = new MatTableDataSource<any>(this.cartas);
 
   ngOnInit(): void {
-    this.cartas = this.inventarioService.getCartas(); // ← devuelve el arreglo directamente
-  this.dataSource = new MatTableDataSource(this.cartas);
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
+    // this.cartas = this.inventarioService.getCartas(); // ← devuelve el arreglo directamente
     
-  }
-  
+    this._inventario.getInventario().subscribe(e => {
+      console.log(e);
+    })
 
-   updatePageSize(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newSize = parseInt(input.value, 10);
-    if (newSize > 0) {
-      this.pageSize = newSize;
-      this.paginator.pageSize = newSize;
-      this.paginator.firstPage();
-    }
   }
 
-  agregarAlCarrito(carta: Carta): void {
-    this.carritoService.agregar(carta);
-    console.log('Producto agregado', this.cartas);
+  getInventario(){
+    this._inventario.getInventario().subscribe(e => {
+      console.log(e)
+    })
   }
-
-  
-
-applyFilter(event: Event): void {
-  const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
-  this.cartasFiltradas = this.cartas.filter(carta =>
-    (carta.nombre?.toLowerCase().includes(filterValue) || '') ||
-    (carta.descripcion?.toLowerCase().includes(filterValue) || '') ||
-    String(carta.cantidad).includes(filterValue) ||
-    (carta.estado ? 'activo' : 'inactivo').includes(filterValue)
-  );
-}
-
-
-
 
 }
