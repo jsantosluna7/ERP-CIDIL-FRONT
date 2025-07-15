8a47f4ba6659c94c4a13a63496e849cbd26343f3
Joan Santos <jsantosluna06@gmail.com>
2025-07-14 20:29:48 -0400

    Característica: Integrar el componente de chat de n8n en el layout y en el login, incluyendo estilos y configuración inicial

--- src/app/components/home/layout/layout.component.ts
+++ src/app/components/home/layout/layout.component.ts
@@ -1,19 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from "../../elements/sidebar/sidebar.component";
-import { NavbarComponent } from "../../elements/navbar/navbar.component";
+import { SidebarComponent } from '../../elements/sidebar/sidebar.component';
+import { NavbarComponent } from '../../elements/navbar/navbar.component';
+import { createChat } from '@n8n/chat';
 
 @Component({
   selector: 'app-layout',
   imports: [CommonModule, RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class LayoutComponent {
+export class LayoutComponent implements AfterViewInit {
   isSidebarClosed = true;
 
   toggleSidebar() {
     this.isSidebarClosed = !this.isSidebarClosed;
+  }
+
+  ngAfterViewInit(): void {
+    createChat({
+      webhookUrl:
+        'http://10.122.120.87:8765/webhook/a889d2ae-2159-402f-b326-5f61e90f602e/chat',
+      webhookConfig: {
+        method: 'POST',
+      },
+      target: '#n8n-chat',
+      mode: 'window',
+      chatInputKey: 'chatInput',
+      showWelcomeScreen: false,
+      defaultLanguage: 'en',
+      initialMessages: [
+        '¡Hola! 👋',
+        'Mi nombre es LIA. ¿Cómo puedo ayudarte hoy?',
+      ],
+      i18n: {
+        en: {
+          title: '¡Bienvenido/a! 👋',
+          subtitle: '',
+          footer: '',
+          getStarted: 'Nueva Conversación',
+          inputPlaceholder: 'Haz tu pregunta..',
+          closeButtonTooltip: 'true',
+        },
+      },
+    });
   }
 }
--- src/app/components/inicio-sesion/login-layout/login-layout.component.ts
+++ src/app/components/inicio-sesion/login-layout/login-layout.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createChat } from '@n8n/chat';
 
 @Component({
   selector: 'app-login-layout',
   imports: [RouterOutlet],
   templateUrl: './login-layout.component.html',
-  styleUrl: './login-layout.component.css'
+  styleUrl: './login-layout.component.css',
 })
-export class LoginLayoutComponent {
-
+export class LoginLayoutComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    createChat({
+      webhookUrl:
+        'http://10.122.120.87:8765/webhook/a889d2ae-2159-402f-b326-5f61e90f602e/chat',
+      webhookConfig: {
+        method: 'POST',
+      },
+      target: '#n8n-chat',
+      mode: 'window',
+      chatInputKey: 'chatInput',
+      showWelcomeScreen: false,
+      defaultLanguage: 'en',
+      initialMessages: [
+        '¡Hola! 👋',
+        'Mi nombre es LIA. ¿Cómo puedo ayudarte hoy?',
+      ],
+      i18n: {
+        en: {
+          title: '¡Bienvenido/a! 👋',
+          subtitle: '',
+          footer: '',
+          getStarted: 'Nueva Conversación',
+          inputPlaceholder: 'Haz tu pregunta..',
+          closeButtonTooltip: 'true',
+        },
+      },
+    });
+  }
 }
