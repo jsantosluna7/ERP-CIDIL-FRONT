4b685a4f8fd26c15d4eb399e3ba416ee71c5e888
Joan Santos <jsantosluna06@gmail.com>
2025-07-16 15:50:15 -0400

    Característica: Rediseñar el componente de la barra lateral con nuevos estilos y funcionalidad de menú desplegable; mejorar la responsividad y la experiencia del usuario.

--- src/app/components/elements/sidebar/sidebar.component.ts
+++ src/app/components/elements/sidebar/sidebar.component.ts
@@ -1,40 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
-  faClock,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AppCualRolDirective } from '../../../directives/app-cual-rol.directive';
+import { MatIconModule } from '@angular/material/icon';
+import {
   faArrowRightFromBracket,
   faBell,
+  faCalendar,
   faCartShopping,
   faChartSimple,
+  faClock,
   faDesktop,
+  faFlask,
+  faGear,
   faGreaterThan,
-  faHeart,
   faHouse,
   faMagnifyingGlass,
   faMicrochip,
   faShop,
-  faUser,
-  faCalendar,
-  faFlask,
   faToolbox,
+  faUser,
   faWarehouse,
 } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UsuariosService } from '../../../services/Api/Usuarios/usuarios.service';
-import { AppCualRolDirective } from '../../../directives/app-cual-rol.directive';
-import { ConfiguracionComponent } from '../navbar/configuracion/configuracion.component';
 import { MatDialog } from '@angular/material/dialog';
-import { Usuarios } from '../../../interfaces/usuarios.interface';
 import { take } from 'rxjs';
+import { ConfiguracionComponent } from '../navbar/configuracion/configuracion.component';
+
+interface DropdownItem {
+  label: string;
+  icon: string;
+  isOpen: boolean;
+  links: string[];
+  height?: string;
+}
 
 @Component({
   selector: 'app-sidebar',
-  imports: [FontAwesomeModule, CommonModule, RouterLink, AppCualRolDirective],
+  imports: [
+    CommonModule,
+    RouterLink,
+    AppCualRolDirective,
+    MatIconModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
   arrowright = faGreaterThan;
   house = faHouse;
   solid = faMagnifyingGlass;
@@ -47,23 +68,14 @@ export class SidebarComponent implements OnInit {
   iot = faMicrochip;
   faclok = faClock;
   carro = faCartShopping;
-   laboratorio = faFlask;
+  laboratorio = faFlask;
   equipo = faToolbox;
   calendar = faCalendar;
   inventario = faWarehouse;
-
-  // body = document.querySelector(".body");k
-  //  sidebar=this.body?.querySelector(".sidebar");
-  //  toggle = this.body?.querySelector(".toggle");
-  //  searchBtn = this.body?.querySelector(".search-box");
-
-  // toogle(event: any){
-  //   const elemento:any = document.querySelector(".sidebar");
-  //   elemento.classList.toggle("close");
-
-  // }
+  config = faGear;
 
   constructor(
+    private elRef: ElementRef,
     private _usuarios: UsuariosService,
     private _router: Router,
     private dialog: MatDialog,
@@ -78,17 +90,9 @@ export class SidebarComponent implements OnInit {
       if (usuario) {
         this.usuarioLogueado = usuario;
 
-        this.nombreUsuario = `${usuario.nombreUsuario} ${usuario.apellidoUsuario}`
+        this.nombreUsuario = `${usuario.nombreUsuario} ${usuario.apellidoUsuario}`;
       }
     });
-  }
-
-  @Output() toggleSidebar = new EventEmitter<void>();
-
-  onToggleClick() {
-    this.toggleSidebar.emit();
-    const elemento: any = document.querySelector('.sidebar');
-    elemento.classList.toggle('close');
   }
 
   salir() {
@@ -105,7 +109,76 @@ export class SidebarComponent implements OnInit {
     dialogRef
       .afterClosed()
       .pipe(take(1))
-      .subscribe((result) => {
+      .subscribe((result) => {});
+  }
+
+  ngAfterViewInit(): void {
+    const toggleDropdown = (
+      dropdown: HTMLElement,
+      menu: HTMLElement,
+      isOpen: boolean
+    ): void => {
+      dropdown.classList.toggle('open', isOpen);
+      if (isOpen) {
+        menu.style.height = menu.scrollHeight + 'px';
+        // fuerza recálculo y luego vuelve a auto
+        setTimeout(() => (menu.style.height = 'auto'), 300);
+      } else {
+        menu.style.height = menu.scrollHeight + 'px';
+        // fuerza que vuelva a 0 con transición
+        requestAnimationFrame(() => (menu.style.height = '0'));
+      }
+    };
+
+    const closeAllDropdowns = (): void => {
+      const openDropdowns = this.elRef.nativeElement.querySelectorAll(
+        '.dropdown-container-sd.open'
+      );
+      openDropdowns.forEach((openDropdown: Element) => {
+        const menu = openDropdown.querySelector(
+          '.dropdown-menu-sd'
+        ) as HTMLElement;
+        if (menu) {
+          toggleDropdown(openDropdown as HTMLElement, menu, false);
+        }
       });
+    };
+
+    const dropdownToggles = this.elRef.nativeElement.querySelectorAll(
+      '.dropdown-toggle-sd'
+    );
+    dropdownToggles.forEach((dropdownToggle: Element) => {
+      dropdownToggle.addEventListener('click', (e: Event) => {
+        e.preventDefault();
+        const dropdown = dropdownToggle.closest(
+          '.dropdown-container-sd'
+        ) as HTMLElement;
+        const menu = dropdown.querySelector('.dropdown-menu-sd') as HTMLElement;
+        const isOpen = dropdown.classList.contains('open');
+        closeAllDropdowns();
+        toggleDropdown(dropdown, menu, !isOpen);
+      });
+    });
+
+    const sidebarButtons = this.elRef.nativeElement.querySelectorAll(
+      '.sidebar-toggler-sd, .sidebar-menu-button'
+    );
+    sidebarButtons.forEach((button: Element) => {
+      button.addEventListener('click', () => {
+        closeAllDropdowns();
+        const sidebar = this.elRef.nativeElement.querySelector(
+          '.sidebar'
+        ) as HTMLElement;
+        sidebar?.classList.toggle('collapsed');
+      });
+    });
+
+    // Collapse on small screens
+    const sidebar = this.elRef.nativeElement.querySelector(
+      '.sidebar'
+    ) as HTMLElement;
+    if (window.innerWidth <= 1024 && sidebar) {
+      sidebar.classList.add('collapsed');
+    }
   }
 }
