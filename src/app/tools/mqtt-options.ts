bf5fdd290c838671be82cfb1da49cfd16e0dc802
jsantosluna7 <jsantosluna06@gmail.com>
2026-02-02 20:10:03 -0400

    feat(compras): eliminar el puerto predeterminado de las opciones del servicio MQTT

--- src/app/tools/mqtt-options.ts
+++ src/app/tools/mqtt-options.ts
@@ -2,7 +2,6 @@ import { IMqttServiceOptions } from 'ngx-mqtt';
 
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
     hostname: process.env['HOSTNAME'],
-    port: 9001,
     path: process.env['PATH'],
     protocol: 'wss'
 }
