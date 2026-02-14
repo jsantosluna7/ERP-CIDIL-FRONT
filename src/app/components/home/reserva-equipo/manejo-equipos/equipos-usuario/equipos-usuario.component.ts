b27423f4fd391ed2fe8f7c382da5c400e84e0468
jsantosluna7 <jsantosluna06@gmail.com>
2026-02-14 17:00:47 -0400

    feat(compras): agregar componente EquiposUsuario con HTML, CSS y archivos de prueba

---
+++ src/app/components/home/reserva-equipo/manejo-equipos/equipos-usuario/equipos-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipos-usuario',
+  imports: [],
+  templateUrl: './equipos-usuario.component.html',
+  styleUrl: './equipos-usuario.component.css'
+})
+export class EquiposUsuarioComponent {
+
+}
